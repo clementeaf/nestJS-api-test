ac8332e3b99b8051a271c30d40b82d00a6e27de8
Clemente <carriagadafalcone@gmail.com>
2023-10-27 18:03:01 -0300

    Deleted module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,8 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommitHistoryModule } from './commit-history/commit-history.module';
 import { WebsocketModule } from './websocket/websocket.module';
 import { GithubApiModule } from './github-api/github-api.module';
-import { AuthModule } from './auth/auth.module';
-import { AuthController } from './auth/auth.controller';
-import { RedirectMiddleware } from './auth/redirect.middleware';
 
 @Module({
   imports: [
@@ -20,19 +12,6 @@ import { RedirectMiddleware } from './auth/redirect.middleware';
     CommitHistoryModule,
     WebsocketModule,
     GithubApiModule,
-    AuthModule,
   ],
-  controllers: [AuthController],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(RedirectMiddleware)
-      .forRoutes(
-        { path: 'main', method: RequestMethod.ALL },
-        { path: '/', method: RequestMethod.ALL },
-        { path: '/repositoryInfo', method: RequestMethod.ALL },
-        { path: '/commits', method: RequestMethod.ALL },
-      );
-  }
-}
+export class AppModule {}
