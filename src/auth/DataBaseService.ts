623e6b8b07eef013c17d294dda260a690911db22
Clemente <carriagadafalcone@gmail.com>
2023-10-27 14:16:25 -0300

    DataBaseService and github Strategy fix

--- src/auth/DataBaseService.ts
+++ src/auth/DataBaseService.ts
@@ -6,10 +6,7 @@ export class DataBaseService {
   static users: {
     find(arg0: (user: any) => boolean): unknown;
     length: number;
-    push(newUser: {
-      id: any; // Esto es solo un ejemplo
-      githubId: any;
-    }): unknown;
+    push(newUser: { id: any; githubId: any }): unknown;
     githubId: string;
   };
 
@@ -18,13 +15,9 @@ export class DataBaseService {
   }
 
   static async createUserFromGitHubData(githubUser: { id: any }) {
-    // Accede a los métodos y propiedades estáticas a través de "this"
-    // Aquí, deberías crear un nuevo usuario en tu base de datos y almacenar la información relevante
-    // Luego, devuelve el usuario recién creado
     const newUser = {
-      id: this.users.length + 1, // Esto es solo un ejemplo
-      githubId: githubUser.id, // El identificador único de GitHub
-      // Otros datos que deseas almacenar
+      id: this.users.length + 1,
+      githubId: githubUser.id,
     };
     this.users.push(newUser);
     return newUser;
