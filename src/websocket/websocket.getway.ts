d639843a1c4e137779da447948a36065d9fe312a
Clemente <carriagadafalcone@gmail.com>
2023-10-28 20:40:49 -0300

    WwebSocket solved

--- src/websocket/websocket.getway.ts
+++ src/websocket/websocket.getway.ts
@@ -1,26 +1,41 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
-  SubscribeMessage,
-  MessageBody,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class WebsocketGateway {
-  @WebSocketServer()
-  server!: Server;
+@WebSocketGateway({
+  cors: { origin: '*' },
+})
+export class WebsocketGateway
+  implements OnModuleInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server!: Server;
 
-  handleConnection(client: Socket) {
-    console.log(`Cliente conectado: ${client.id}`);
+  onModuleInit() {
+    console.log('Server initialization');
   }
 
-  handleEvent(data: any) {
-    this.server.emit('evento', data);
+  handleConnection(client: Socket) {
+    console.log(`Cliente conectado`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Cliente conectado`);
   }
 
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() data: string): void {
-    this.server.emit('message', data);
+  handleMessage(
+    @MessageBody() data: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    // Handle received message
+    this.server.emit('message', data); // Broadcast the message to all connected clients
   }
 }
