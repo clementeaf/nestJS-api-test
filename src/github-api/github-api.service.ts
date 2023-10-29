2b2c9866febbdba0ab3f753f266f0b89ca6f457f
Clemente <carriagadafalcone@gmail.com>
2023-10-29 15:05:44 -0300

    brief intervention on github.service

--- src/github-api/github-api.service.ts
+++ src/github-api/github-api.service.ts
@@ -8,10 +8,7 @@ export class GithubApiService {
   private wss: Server;
 
   constructor() {
-    const accessToken = '24115446489b35a8d88a2f5e013edf79f54c7a4d';
-    this.octokit = new Octokit({
-      auth: accessToken,
-    });
+    this.octokit = new Octokit();
     this.wss = new Server({ noServer: true });
 
     this.wss.on('connection', (ws) => {
