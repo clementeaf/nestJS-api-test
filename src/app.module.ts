042779dececc713623d75fadbb568c88fbb051cd
Clemente <carriagadafalcone@gmail.com>
2023-10-29 17:54:51 -0300

    websocket removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommitHistoryModule } from './commit-history/commit-history.module';
-import { WebsocketModule } from './websocket/websocket.module';
 import { GithubApiModule } from './github-api/github-api.module';
 
 @Module({
@@ -10,7 +9,6 @@ import { GithubApiModule } from './github-api/github-api.module';
       'mongodb+srv://clemente:clemente@blazorappdb.j8v7ctn.mongodb.net/blazorappdb',
     ),
     CommitHistoryModule,
-    WebsocketModule,
     GithubApiModule,
   ],
 })
--- src/websocket/websocket.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WebsocketGateway } from './websocket.getway';
-
-@Module({
-  providers: [WebsocketGateway],
-})
-export class WebsocketModule {}
