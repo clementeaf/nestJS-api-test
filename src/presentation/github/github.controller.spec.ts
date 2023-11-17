a5f7182b340d573f454c747c1bfbbb9f752411b2
clemente <carriagadafalcone@gmail.com>
2023-11-17 18:11:50 -0300

    github.controller.spec.ts documentation

--- src/presentation/github/github.controller.spec.ts
+++ src/presentation/github/github.controller.spec.ts
@@ -6,14 +6,20 @@ describe('GithubController', () => {
   let controller: GithubController;
 
   beforeEach(async () => {
+    // Set up the testing module with the necessary controller and service
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GithubController],
       providers: [ApplicationGithubService],
     }).compile();
 
+    // Obtain an instance of the controller for testing
     controller = module.get<GithubController>(GithubController);
   });
 
+  /**
+   * Test if the GithubController is defined.
+   * This test ensures that the controller instance is created successfully.
+   */
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
