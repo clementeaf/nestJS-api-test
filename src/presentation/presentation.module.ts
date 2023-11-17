6b533d926ae5a8efe7ba2a9a2aa43bd070432666
clemente <carriagadafalcone@gmail.com>
2023-11-17 18:12:53 -0300

    presentation.module.ts documentation

--- src/presentation/presentation.module.ts
+++ src/presentation/presentation.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GithubController } from './github/github.controller';
 
+/**
+ * Presentation module for organizing controllers related to Github presentation.
+ * This module encapsulates the GithubController for handling presentation logic.
+ */
 @Module({
   controllers: [GithubController],
 })
