23a2136b8d6fc6d2685cbe0dfa1e479434cc36b7
clemente <carriagadafalcone@gmail.com>
2023-11-17 18:53:07 -0300

    github.dto.ts documentation

--- src/application/github/github.dto/github.dto.ts
+++ src/application/github/github.dto/github.dto.ts
@@ -1,9 +1,44 @@
-// application/github/github.dto.ts
+import { IsNotEmpty, IsString } from 'class-validator';
 
+/**
+ * Data transfer object for repository information.
+ */
 export class RepoInfoDto {
-  name: string | undefined;
+  /**
+   * The name of the repository.
+   */
+  @IsString({ message: 'Name should be a string' })
+  @IsNotEmpty({ message: 'Name should not be empty' })
+  name: string = '';
 }
 
+/**
+ * Data transfer object for commit information.
+ */
 export class CommitDto {
-  sha: string | undefined;
+  /**
+   * The SHA (Secure Hash Algorithm) of the commit.
+   */
+  @IsString({ message: 'SHA should be a string' })
+  @IsNotEmpty({ message: 'SHA should not be empty' })
+  sha: string = '';
+}
+
+/**
+ * Data transfer object for creating a new commit.
+ */
+export class CreateCommitDto {
+  /**
+   * The commit message.
+   */
+  @IsString({ message: 'Message should be a string' })
+  @IsNotEmpty({ message: 'Message should not be empty' })
+  message: string = '';
+
+  /**
+   * The author of the commit.
+   */
+  @IsString({ message: 'Author should be a string' })
+  @IsNotEmpty({ message: 'Author should not be empty' })
+  author: string = '';
 }
