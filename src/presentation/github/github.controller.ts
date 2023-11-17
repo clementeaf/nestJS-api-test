558d6602ae719348d6ed6f95d6f8a29948e9988d
clemente <carriagadafalcone@gmail.com>
2023-11-17 18:13:52 -0300

    github.controller.ts documentation

--- src/presentation/github/github.controller.ts
+++ src/presentation/github/github.controller.ts
@@ -5,15 +5,31 @@ import {
   RepoInfoDto,
 } from '../../application/github/github.dto/github.dto';
 
+/**
+ * Controller responsible for handling Github-related HTTP requests.
+ * Endpoints:
+ * - GET /github/repo-info: Retrieve information about the Github repository.
+ * - GET /github/commits: Retrieve a list of commits from the Github repository.
+ */
 @Controller('github')
 export class GithubController {
   constructor(private readonly githubService: ApplicationGithubService) {}
 
+  /**
+   * Endpoint: GET /github/repo-info
+   * Retrieve information about the Github repository.
+   * @returns {Promise<RepoInfoDto>} Information about the Github repository.
+   */
   @Get('repo-info')
   async getRepoInfo(): Promise<RepoInfoDto> {
     return this.githubService.getRepoInfo();
   }
 
+  /**
+   * Endpoint: GET /github/commits
+   * Retrieve a list of commits from the Github repository.
+   * @returns {Promise<CommitDto[]>} List of commits from the Github repository.
+   */
   @Get('commits')
   async getCommits(): Promise<CommitDto[]> {
     return this.githubService.getCommits();
