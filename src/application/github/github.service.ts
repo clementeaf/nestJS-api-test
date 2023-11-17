7ad27363ea9f8e634cee591f56db7fcff063a746
clemente <carriagadafalcone@gmail.com>
2023-11-17 18:10:07 -0300

    github.service.ts documentation

--- src/application/github/github.service.ts
+++ src/application/github/github.service.ts
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // application/github/github.service.ts
 
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { GitHubConnection } from '../../infrastructure/github/github/github';
 import { CommitDto, RepoInfoDto } from './github.dto/github.dto';
 
@@ -13,30 +17,63 @@ export class ApplicationGithubService {
     this.gitHubConnection = new GitHubConnection();
   }
 
+  /**
+   * Get repository information.
+   * @returns {Promise<RepoInfoDto>} Repository information.
+   * @throws {NotFoundException} If the repository is not found.
+   * @throws {InternalServerErrorException} If an internal server error occurs.
+   */
   async getRepoInfo(): Promise<RepoInfoDto> {
     const owner = 'clementeaf';
     const repo = 'nestJS-api-test';
 
-    const repoInfo = await this.gitHubConnection.getRepoInfo(owner, repo);
+    try {
+      const repoInfo = await this.gitHubConnection.getRepoInfo(owner, repo);
 
-    const repoInfoDto: RepoInfoDto = {
-      name: repoInfo?.name,
-    };
+      if (!repoInfo) {
+        throw new NotFoundException('Repository not found');
+      }
 
-    return repoInfoDto;
+      const repoInfoDto: RepoInfoDto = {
+        name: repoInfo.name,
+      };
+
+      return repoInfoDto;
+    } catch (error) {
+      console.error(error);
+      throw new InternalServerErrorException(
+        'Error fetching repository information',
+      );
+    }
   }
 
+  /**
+   * Get list of commits.
+   * @returns {Promise<CommitDto[]>} List of commits.
+   * @throws {NotFoundException} If commits are not found.
+   * @throws {InternalServerErrorException} If an internal server error occurs.
+   */
   async getCommits(): Promise<CommitDto[]> {
     const owner = 'clementeaf';
     const repo = 'nestJS-api-test';
-    const commits = await this.gitHubConnection.getCommits(owner, repo);
 
-    const commitsDto: CommitDto[] = (commits || []).map((commit) => {
-      return {
-        sha: commit.sha,
-      };
-    });
+    try {
+      const commits = await this.gitHubConnection.getCommits(owner, repo);
 
-    return commitsDto;
+      if (!commits) {
+        throw new NotFoundException('Commits not found');
+      }
+
+      const commitsDto: CommitDto[] = (commits || []).map((commit) => {
+        return {
+          sha: commit.sha,
+        };
+      });
+
+      return commitsDto;
+    } catch (error) {
+      console.error(error);
+      throw new InternalServerErrorException('Error fetching commits');
+    }
   }
 }
