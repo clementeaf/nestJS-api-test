707562cdc0ae33fcedd99bc15436265ac97a2f6a
clemente <carriagadafalcone@gmail.com>
2023-11-17 19:05:04 -0300

    app.module.ts documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
 import { GithubApiModule } from './github-api/github-api.module';
 import { ConfigModule } from '@nestjs/config';
-import { ApplicationModule } from './application/application.module';
 import { PresentationModule } from './presentation/presentation.module';
 import { InfrastructureModule } from './infrastructure/infrastructure.module';
 import { ApplicationModule } from './application/application.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), GithubApiModule, ApplicationModule, InfrastructureModule, PresentationModule],
+  imports: [
+    // Load environment variables using the ConfigModule
+    ConfigModule.forRoot(),
+
+    // Import the GithubApiModule to enable GitHub API-related features
+    GithubApiModule,
+
+    // Import the ApplicationModule where your application's core functionality is defined
+    ApplicationModule,
+
+    // Import the InfrastructureModule to handle low-level services like database connections
+    InfrastructureModule,
+
+    // Import the PresentationModule where your controllers and application's HTTP layer are defined
+    PresentationModule,
+  ],
 })
 export class AppModule {}
