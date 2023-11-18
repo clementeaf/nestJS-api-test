d9a0f05f4e1b528a6e7cdccca40283c9f578cd71
clemente <carriagadafalcone@gmail.com>
2023-11-18 16:07:52 -0300

    end-to-end test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import request from 'supertest'; 
+import { AppModule } from '../src/app.module';
+import { CommitDto } from '../src/application/github/dto/github.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,37 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/github/repo-info (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/github/repo-info')
       .expect(200)
-      .expect('Hello World!');
+      .expect({
+        name: 'nestJS-api-test'
+      });
+  });
+
+  it('/github/commits (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/github/commits')
+      .expect(200)
+      .expect((response) => {
+        const commits = response.body as CommitDto[];
+        expect(commits.length).toBeGreaterThan(0);
+  
+        const expectedCommit = {
+          sha: '61caa95983d12a0f6ba414796096cb4ba1bcb80f',
+          commit: {
+            author: {
+              name: 'clemente',
+              email: 'carriagadafalcone@gmail.com',
+              date: '2023-11-18T17:28:22Z',
+            },
+          },
+        };
+  
+        const matchingCommit = commits.find(commit => commit.sha === expectedCommit.sha);
+        expect(matchingCommit).toBeDefined();
+        expect(matchingCommit).toMatchObject(expectedCommit);
+      });
   });
 });
